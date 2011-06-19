553494ee5409e7e468534fcac32eda7c6e9cf962
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-06-19 19:45:43 +0000

    added MC and b-jet bins

--- scripts/QCDEstimation.py
+++ scripts/QCDEstimation.py
@@ -1,6 +1,7 @@
 from ROOT import *
-from HistGetter import *
-from HistPlotter import *
+
+from HistPlotter import inclusiveBjetBins, exclusiveBjetBins, allBjetBins
+from HistGetter import getHistsFromFiles,addSampleSum
 
 def doFit(histogram, function, fitRange):#, constrainFit = False):
     numberOfFreeParameters = -1
@@ -20,32 +21,29 @@ def getEstimate(fit, signalRegion = (0.,0.1), binWidthOfOriginalHIstoram = 0.1, 
     return estimate
 
 
-def getQCDEstimate(histname):
-    files = {'data':"/storage/results/2011/new/data_349.007pb_PFElectron_PF2PATJets_PFMET.root"}
-    hist = 'QCDStudy/PFIsolation_WithMETCutAndAsymJetCuts'
+def getQCDEstimate(histname, datafile):
+    files = {'data': datafile}
+    histogramForEstimation = 'QCDStudy/PFIsolation_WithMETCutAndAsymJetCuts'
     
-    suffixes = ["0orMoreBtag",'0btag',
-        "1orMoreBtag",
-        "2orMoreBtags",
-        "3orMoreBtags",'4orMoreBtags' ]
+    suffixes = allBjetBins
     
     binWidth = 0.01
     rebin = 10
     function = 'gaus'
     signalRegion = (0.,0.1)
-    estimate = 0
+    estimate = -1
     
     for suffix in suffixes:
         if suffix in histname:
-            hist = hist + '_' + suffix
-            hists = [hist]
+            histogramForEstimation = histogramForEstimation + '_' + suffix
+            hists = [histogramForEstimation]
             hists = getHistsFromFiles( hists, files )
-            hist = hists['data'][hist]
-            hist.Rebin(rebin)
-            fit1 = doFit(hist, function, ( 0.1, 1.1 ))
+            histogramForEstimation = hists['data'][histogramForEstimation]
+            histogramForEstimation.Rebin(rebin)
+            fit1 = doFit(histogramForEstimation, function, ( 0.1, 1.1 ))
             if fit1:
                 fit1 = fit1.Clone()
-            fit2 = doFit(hist, function, ( 0.2, 1.1 ))
+            fit2 = doFit(histogramForEstimation, function, ( 0.2, 1.1 ))
             if fit2:
                 fit2 = fit2.Clone()
             
@@ -64,22 +62,40 @@ def getQCDEstimate(histname):
 if __name__ == '__main__':
     gROOT.SetBatch( True )
     gROOT.ProcessLine( 'gErrorIgnoreLevel = 1001;' )
-    files = {'data':"/storage/results/2011/new/data_349.007pb_PFElectron_PF2PATJets_PFMET.root"}
+    files = {
+    'data':"/storage/results/histogramFiles/ElectronHad_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'bce1' :  "/storage/results/histogramFiles/QCD_Pt-20to30_BCtoE_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'bce2' :  "/storage/results/histogramFiles/QCD_Pt-30to80_BCtoE_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'bce3' :  "/storage/results/histogramFiles/QCD_Pt-80to170_BCtoE_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'enri1' : "/storage/results/histogramFiles/QCD_Pt-20to30_EMEnriched_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'enri2' :  "/storage/results/histogramFiles/QCD_Pt-30to80_EMEnriched_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'enri3' :  "/storage/results/histogramFiles/QCD_Pt-80to170_EMEnriched_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'pj1' :  "/storage/results/histogramFiles/GJets_TuneD6T_HT-100To200_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'pj2' :  "/storage/results/histogramFiles/GJets_TuneD6T_HT-100To200_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+    'pj3' :  "/storage/results/histogramFiles/GJets_TuneD6T_HT-200_715.09pb_PFElectron_PF2PATJets_PFMET.root",
+             }
+
     hists = ['QCDStudy/PFIsolation_WithMETCutAndAsymJetCuts']
     
-    suffixes = HistPlotter.inclusiveBjetBins
+    suffixes = allBjetBins
     hists = [hist + '_' + suffix for hist in hists for suffix in suffixes]
     
     hists = getHistsFromFiles( hists, files )
+    hists = addSampleSum(hists)
     binWidth = 0.01
     rebin = 10
-    function = 'gaus'
+    function = 'pol1'
     signalRegion = (0.,0.1)
     
+    print 'QCD'
+    for name,hist in hists['qcd'].iteritems():
+        print name
+        hist.Rebin(rebin)
+        print 'signal bin', hist.GetBinContent(1)
     
-    
+    print 'DATA', function
     for name,hist in hists['data'].iteritems():
-        print name, hist.GetEntries()
+        print name
         hist.Rebin(rebin)
         print 'signal bin', hist.GetBinContent(1)
         fit1 = doFit(hist, function, ( 0.1, 1.1 ))
