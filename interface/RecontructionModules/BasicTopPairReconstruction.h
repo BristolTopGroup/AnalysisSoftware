4fa178806b2e7d244b547d44af06a701a056757c
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-09-05 10:58:21 +0000

    added initial criteria for reconstruction which are now checked in the contructor TODO: replace the exception with a more detailed one

--- interface/RecontructionModules/BasicTopPairReconstruction.h
+++ interface/RecontructionModules/BasicTopPairReconstruction.h
@@ -10,7 +10,6 @@
 
 #include <boost/shared_ptr.hpp>
 #include "../TtbarHypothesis.h"
-#include "BasicNeutrinoReconstruction.h"
 
 namespace BAT {
 
@@ -18,25 +17,25 @@ class BasicTopPairReconstruction {
 public:
 	BasicTopPairReconstruction(const ElectronPointer electron, const METPointer met, const JetCollection jets);
 	virtual ~BasicTopPairReconstruction();
-	const TtbarHypothesisPointer getBestSolution();
-	std::vector<TtbarHypothesisPointer> getAllSolutions();
+	virtual const TtbarHypothesisPointer getBestSolution();
+	virtual std::vector<TtbarHypothesisPointer> getAllSolutions();
 
 protected:
-	double getDiscriminator(const TtbarHypothesisPointer) const;
-	void reconstruct();
-	bool meetsHadronicBJetRequirement(unsigned short jetIndex);
-	bool meetsLeptonicBJetRequirement(unsigned short jetIndex);
-	bool meetsJetFromWRequirement(unsigned short jet1Index, unsigned short jet2Index);
-	bool meetsGlobalRequirement(const TtbarHypothesisPointer solution);
+	virtual double getDiscriminator(const TtbarHypothesisPointer) const;
+	virtual void reconstruct();
+	virtual bool meetsHadronicBJetRequirement(unsigned short jetIndex);
+	virtual bool meetsLeptonicBJetRequirement(unsigned short jetIndex);
+	virtual bool meetsJetFromWRequirement(unsigned short jet1Index, unsigned short jet2Index);
+	virtual bool meetsGlobalRequirement(const TtbarHypothesisPointer solution);
+	virtual bool meetsInitialCriteria();
+	virtual boost::array<ParticlePointer, 2> getNeutrinos();
 
 protected:
 	std::vector<TtbarHypothesisPointer> solutions;
 	bool alreadyReconstructed;
-	compare_disc solutionComparator;
 	METPointer met;
 	JetCollection jets;
 	ElectronPointer electronFromW;
-	BasicNeutrinoReconstruction neutrinoReconstruction;
 };
 
 typedef boost::shared_ptr<BasicTopPairReconstruction> TopPairReconstruction;
--- src/RecontructionModules/BasicTopPairReconstruction.cpp
+++ src/RecontructionModules/BasicTopPairReconstruction.cpp
@@ -6,18 +6,23 @@
  */
 
 #include "../../interface/RecontructionModules/BasicTopPairReconstruction.h"
+#include "../../interface/RecontructionModules/BasicNeutrinoReconstruction.h"
 
 namespace BAT {
 
 BasicTopPairReconstruction::BasicTopPairReconstruction(const ElectronPointer electron, const METPointer met, const JetCollection jets):
 		solutions(),
 		alreadyReconstructed(false),
-		solutionComparator(),
 		met(met),
 		jets(jets),
-		electronFromW(electron),
-		neutrinoReconstruction(electron, met){
+		electronFromW(electron) {
+	if(!meetsInitialCriteria()){
+		throw ReconstructionException("Initial Criteria not met");//replace with detailed msg
+	}
+}
 
+bool BasicTopPairReconstruction::meetsInitialCriteria() {
+	return met != 0 && electronFromW != 0 && jets.size() >= 4;
 }
 
 BasicTopPairReconstruction::~BasicTopPairReconstruction() {
@@ -27,6 +32,7 @@ std::vector<TtbarHypothesisPointer> BasicTopPairReconstruction::getAllSolutions(
 	if(!alreadyReconstructed)
 		reconstruct();
 
+	SmallerDiscriminatorIsBetter solutionComparator = SmallerDiscriminatorIsBetter();
 	std::sort(solutions.begin(), solutions.end(), solutionComparator);
 	return solutions;
 }
@@ -36,51 +42,60 @@ const TtbarHypothesisPointer BasicTopPairReconstruction::getBestSolution() {
 }
 
 void BasicTopPairReconstruction::reconstruct() {
-	ParticlePointer neutrino = neutrinoReconstruction.getNeutrinos(NeutrinoSelection::mostCentral)[0];
-
+	boost::array<ParticlePointer, 2> neutrinos = getNeutrinos();
 	typedef unsigned short ushort;
 
-	for (ushort hadBindex = 0; hadBindex < jets.size(); ++hadBindex) {
-		if (!meetsHadronicBJetRequirement(hadBindex))
-			continue;
-
-		for (ushort lepBindex = 0; lepBindex < jets.size(); ++lepBindex) {
-			if (lepBindex == hadBindex || !meetsLeptonicBJetRequirement(lepBindex))
+	for (ushort neutrinoIndex = 0; neutrinoIndex < neutrinos.size(); ++neutrinoIndex) {
+		ParticlePointer neutrino = neutrinos.at(neutrinoIndex);
+		for (ushort hadBindex = 0; hadBindex < jets.size(); ++hadBindex) {
+			if (!meetsHadronicBJetRequirement(hadBindex))
 				continue;
 
-			for (ushort jet1Index = 0; jet1Index < jets.size(); ++jet1Index) {
-				if (jet1Index == hadBindex || jet1Index == lepBindex)
+			for (ushort lepBindex = 0; lepBindex < jets.size(); ++lepBindex) {
+				if (lepBindex == hadBindex || !meetsLeptonicBJetRequirement(lepBindex))
 					continue;
 
-				for (ushort jet2Index = 0; jet2Index < jets.size();
-						++jet2Index) {
-					if (jet2Index == hadBindex || jet2Index == lepBindex
-							|| !meetsJetFromWRequirement(jet1Index, jet2Index))
+				for (ushort jet1Index = 0; jet1Index < jets.size(); ++jet1Index) {
+					if (jet1Index == hadBindex || jet1Index == lepBindex)
 						continue;
-					TtbarHypothesisPointer solution(new TtbarHypothesis());
-					//leptons
-					solution->electronFromW = electronFromW;
-					solution->neutrinoFromW = neutrino;
-					//jets
-					solution->hadronicBJet = jets.at(hadBindex);
-					solution->leptonicBjet = jets.at(lepBindex);
-					solution->jet1FromW = jets.at(jet1Index);
-					solution->jet2FromW = jets.at(jet2Index);
 
-					//combine reconstructed objects
-					solution->combineReconstructedObjects();
+					for (ushort jet2Index = 0; jet2Index < jets.size(); ++jet2Index) {
+						if (jet2Index == hadBindex || jet2Index == lepBindex
+								|| !meetsJetFromWRequirement(jet1Index, jet2Index))
+							continue;
+						TtbarHypothesisPointer solution(new TtbarHypothesis());
+						//leptons
+						solution->electronFromW = electronFromW;
+						solution->neutrinoFromW = neutrino;
+						//jets
+						solution->hadronicBJet = jets.at(hadBindex);
+						solution->leptonicBjet = jets.at(lepBindex);
+						solution->jet1FromW = jets.at(jet1Index);
+						solution->jet2FromW = jets.at(jet2Index);
 
-					solution->disc = getDiscriminator(solution);
+						//combine reconstructed objects
+						solution->combineReconstructedObjects();
 
-					if(meetsGlobalRequirement(solution))
-						solutions.push_back(solution);
-					else
-						continue;
+						solution->discriminator = getDiscriminator(solution);
+
+						if (meetsGlobalRequirement(solution)){
+							solutions.push_back(solution);
+						}
+
+						else
+							continue;
+					}
 				}
 			}
 		}
 	}
 	alreadyReconstructed = true;
+}
+
+
+boost::array<ParticlePointer, 2> BasicTopPairReconstruction::getNeutrinos() {
+	BasicNeutrinoReconstruction neutrinoReconstruction(electronFromW, met);
+	return neutrinoReconstruction.getNeutrinos(NeutrinoSelection::None);
 }
 
 bool BasicTopPairReconstruction::meetsHadronicBJetRequirement(unsigned short jetIndex){
@@ -96,7 +111,7 @@ bool BasicTopPairReconstruction::meetsJetFromWRequirement(unsigned short jet1Ind
 }
 
 bool BasicTopPairReconstruction::meetsGlobalRequirement(const TtbarHypothesisPointer solution){
-	return true;
+	return solution->isPhysical();
 }
 
 double BasicTopPairReconstruction::getDiscriminator(const TtbarHypothesisPointer solution) const{
