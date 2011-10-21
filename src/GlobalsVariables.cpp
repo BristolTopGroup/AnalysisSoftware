116e3f9c76c8723381f60c6e5ef062d62d42c79c
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-10-21 10:58:20 +0000

    added GlobalVariables feature. This will make configuration of the analysis much easier

---
+++ src/GlobalsVariables.cpp
@@ -0,0 +1,12 @@
+/*
+ * Globals.cpp
+ *
+ *  Created on: 20 Oct 2011
+ *      Author: kreczko
+ */
+
+#include "../interface/GlobalVariables.h"
+
+namespace BAT {
+std::string Globals::TQAFPath = "";
+}
