bd1c7087ed06574ce8cf71ae21c6809270062dbe
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-10-21 10:58:06 +0000

    added GlobalVariables feature. This will make configuration of the analysis much easier

---
+++ interface/GlobalVariables.h
@@ -0,0 +1,22 @@
+/*
+ * GlobalVariables.h
+ *
+ *  Created on: 20 Oct 2011
+ *      Author: kreczko
+ */
+
+#ifndef GLOBALVARIABLES_H_
+#define GLOBALVARIABLES_H_
+#include "RecoObjects/Jet.h"
+
+namespace BAT {
+struct Globals {
+
+//static JetAlgorithm::value jetAlgorithm = JetAlgorithm::PF2PAT;
+
+static std::string TQAFPath;
+};
+
+}
+
+#endif /* GLOBALVARIABLES_H_ */
