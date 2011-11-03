0856c7ac4672d18fefd39dd63672ef40c309be98
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-11-03 12:18:15 +0000

    added more global variables

--- interface/GlobalVariables.h
+++ interface/GlobalVariables.h
@@ -8,13 +8,46 @@
 #ifndef GLOBALVARIABLES_H_
 #define GLOBALVARIABLES_H_
 #include "RecoObjects/Jet.h"
+#include "RecoObjects/Electron.h"
+#include "RecoObjects/Muon.h"
+#include "RecoObjects/MET.h"
 
 namespace BAT {
 struct Globals {
 
-//static JetAlgorithm::value jetAlgorithm = JetAlgorithm::PF2PAT;
-
+//general
 static std::string TQAFPath;
+static double luminosity;
+
+
+//electrons
+static double maxElectronPFIsolation;
+static double maxElectronRelativeIsolation;
+static ElectronAlgorithm::value electronAlgorithm;
+static ElectronID::value electronID;
+static double minElectronET;
+static double MaxAbsoluteElectronEta;
+
+
+//muons
+static double maxMuonPFIsolation;
+static double maxMuonRelativeIsolation;
+static MuonAlgorithm::value muonAlgorithm;
+static double maxAbsoluteMuonEta;
+static double minMuonPt;
+
+//jets
+static JetAlgorithm::value jetAlgorithm;
+static BtagAlgorithm::value btagAlgorithm;
+static BtagAlgorithm::workingPoint btagWorkingPoint;
+
+//good jet definition
+static double maxAbsoluteJetEta;
+static double minJetPt;
+
+//MET
+static METAlgorithm::value metAlgorithm;
+static double METCut;
 };
 
 }
--- src/GlobalsVariables.cpp
+++ src/GlobalsVariables.cpp
@@ -8,5 +8,33 @@
 #include "../interface/GlobalVariables.h"
 
 namespace BAT {
+
+//general
 std::string Globals::TQAFPath = "";
+double Globals::luminosity = 3934.085;
+
+//electrons
+double Globals::maxElectronPFIsolation = 0.1;
+double Globals::maxElectronRelativeIsolation = 0.1;
+ElectronAlgorithm::value Globals::electronAlgorithm = ElectronAlgorithm::ParticleFlow;
+ElectronID::value Globals::electronID = ElectronID::CiCHyperTight1MC;
+double Globals::MaxAbsoluteElectronEta = 2.5;
+double Globals::minElectronET = 30;
+
+//muons
+double Globals::maxMuonRelativeIsolation = 0.1;
+double Globals::maxMuonPFIsolation = 0.1;
+MuonAlgorithm::value Globals::muonAlgorithm = MuonAlgorithm::ParticleFlow;
+double Globals::maxAbsoluteMuonEta = 2.1;
+double Globals::minMuonPt = 20.;
+
+//jets
+JetAlgorithm::value Globals::jetAlgorithm = JetAlgorithm::PF2PAT;
+BtagAlgorithm::value Globals::btagAlgorithm = BtagAlgorithm::SimpleSecondaryVertexHighEffBTag;
+BtagAlgorithm::workingPoint Globals::btagWorkingPoint = BtagAlgorithm::MEDIUM;
+double Globals::maxAbsoluteJetEta = 2.4;
+double Globals::minJetPt = 30.;//GeV
+//MET
+METAlgorithm::value Globals::metAlgorithm = METAlgorithm::ParticleFlowMET;
+double Globals::METCut = 20.; //GeV
 }
