1f0ab5645ae5f732fd4de8b66a5bcd760e3ed72e
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-11-03 12:37:26 +0000

    added new variables for muon selection for mu+jets analysis

--- interface/Readers/MuonReader.h
+++ interface/Readers/MuonReader.h
@@ -21,17 +21,34 @@ public:
 	void initialise();
 
 private:
-//	VariableReader<unsigned int> numberOfMuonsReader;
 	VariableReader<MultiDoublePointer> energyReader;
 	VariableReader<MultiDoublePointer> pxReader;
 	VariableReader<MultiDoublePointer> pyReader;
 	VariableReader<MultiDoublePointer> pzReader;
+	VariableReader<MultiIntPointer> chargeReader;
+
+	VariableReader<MultiDoublePointer> d0_BS_Reader;
+	VariableReader<MultiDoublePointer> d0_PV_Reader;
 
 	VariableReader<MultiDoublePointer> ecalIsolationReader;
 	VariableReader<MultiDoublePointer> hcalIsolationReader;
 	VariableReader<MultiDoublePointer> trackerIsolationReader;
 
-	VariableReader<MultiBoolPointer> isGlobalReader;
+	VariableReader<MultiDoublePointer> PFGammaIsolationReader;
+	VariableReader<MultiDoublePointer> PFChargedHadronIsolationReader;
+	VariableReader<MultiDoublePointer> PFNeutralHadronIsolationReader;
+
+	VariableReader<MultiBoolPointer> isGlobalMuonReader;
+	VariableReader<MultiBoolPointer> isTrackerMuonReader;
+	VariableReader<MultiDoublePointer> vertex_dist_z;
+	VariableReader<MultiDoublePointer> normalisedChi2;
+	VariableReader<MultiIntPointer> NumberOfValidHits;
+	VariableReader<MultiIntPointer> NumberOfValidMuonHits;
+	VariableReader<MultiIntPointer> PixelLayersWithMeasurement;
+	VariableReader<MultiIntPointer> NumberOfMatchedStations;
+	VariableReader<MultiIntPointer> NumberOfMatches;
+
+	MuonAlgorithm::value algorithm;
 
 	MuonCollection muons;
 	void readMuons();
--- src/Readers/MuonReader.cpp
+++ src/Readers/MuonReader.cpp
@@ -10,15 +10,29 @@
 namespace BAT {
 
 MuonReader::MuonReader() :
-//	numberOfMuonsReader(),
 	energyReader(),
 	pxReader(),
 	pyReader(),
 	pzReader(),
+	chargeReader(),
+	d0_BS_Reader(),
+	d0_PV_Reader(),
 	ecalIsolationReader(),
 	hcalIsolationReader(),
 	trackerIsolationReader(),
-	isGlobalReader() {
+	PFGammaIsolationReader(),
+	PFChargedHadronIsolationReader(),
+	PFNeutralHadronIsolationReader(),
+	isGlobalMuonReader(),
+	isTrackerMuonReader(),
+	vertex_dist_z(),
+	normalisedChi2(),
+	NumberOfValidHits(),
+	NumberOfValidMuonHits(),
+	PixelLayersWithMeasurement(),
+	NumberOfMatchedStations(),
+	algorithm(MuonAlgorithm::Default),
+	muons(){
 
 }
 
@@ -27,10 +41,26 @@ MuonReader::MuonReader(TChainPointer input, MuonAlgorithm::value algo) :
 	pxReader(input, MuonAlgorithm::prefixes.at(algo) + ".Px"),
 	pyReader(input, MuonAlgorithm::prefixes.at(algo) + ".Py"),
 	pzReader(input, MuonAlgorithm::prefixes.at(algo) + ".Pz"),
+	chargeReader(input, MuonAlgorithm::prefixes.at(algo) + ".Charge"),
+	d0_BS_Reader(input, MuonAlgorithm::prefixes.at(algo) + ".BeamSpotDXY"),
+	d0_PV_Reader(input, MuonAlgorithm::prefixes.at(algo) + ".PrimaryVertexDXY"),
 	ecalIsolationReader(input, MuonAlgorithm::prefixes.at(algo) + ".EcalIso03"),
 	hcalIsolationReader(input, MuonAlgorithm::prefixes.at(algo) + ".HcalIso03"),
 	trackerIsolationReader(input, MuonAlgorithm::prefixes.at(algo) + ".TrkIso03"),
-	isGlobalReader(input, MuonAlgorithm::prefixes.at(algo) + ".isGlobalMuon") {
+	PFGammaIsolationReader(input, MuonAlgorithm::prefixes.at(algo) + ".PFGammaIso03"),
+	PFChargedHadronIsolationReader(input, MuonAlgorithm::prefixes.at(algo) + ".PfChargedHadronIso03"),
+	PFNeutralHadronIsolationReader(input, MuonAlgorithm::prefixes.at(algo) + ".PfNeutralHadronIso03"),
+	isGlobalMuonReader(input, MuonAlgorithm::prefixes.at(algo) + ".isGlobalMuon"),
+	isTrackerMuonReader(input, MuonAlgorithm::prefixes.at(algo) + ".isTrackerMuon"),
+	vertex_dist_z(input, MuonAlgorithm::prefixes.at(algo) + ".Vertex.DistZ"),
+	normalisedChi2(input, MuonAlgorithm::prefixes.at(algo) + ".GlobalTrack.NormalizedChi2"),
+	NumberOfValidHits(input, MuonAlgorithm::prefixes.at(algo) + ".InnerTrack.NumberOfValidHits"),
+	NumberOfValidMuonHits(input, MuonAlgorithm::prefixes.at(algo) + ".GlobalTrack.NumberOfValidMuonHits"),
+	PixelLayersWithMeasurement(input, MuonAlgorithm::prefixes.at(algo) + ".InnerTrack.PixelLayersWithMeasurement"),
+	NumberOfMatchedStations(input, MuonAlgorithm::prefixes.at(algo) + ".NumberOfMatchedStations"),
+	NumberOfMatches(input, MuonAlgorithm::prefixes.at(algo) + ".NumberOfMatches"),
+	algorithm(algo),
+	muons() {
 
 }
 
@@ -39,12 +69,26 @@ void MuonReader::initialise() {
     pxReader.initialise();
     pyReader.initialise();
     pzReader.initialise();
+    chargeReader.initialise();
 
+    d0_BS_Reader.initialise();
+    d0_PV_Reader.initialise();
     ecalIsolationReader.initialise();
     hcalIsolationReader.initialise();
     trackerIsolationReader.initialise();
+    PFGammaIsolationReader.initialise();
+    PFChargedHadronIsolationReader.initialise();
+    PFNeutralHadronIsolationReader.initialise();
 
-    isGlobalReader.initialise();
+    isGlobalMuonReader.initialise();
+    isTrackerMuonReader.initialise();
+    vertex_dist_z.initialise();
+    normalisedChi2.initialise();
+    NumberOfValidHits.initialise();
+    NumberOfValidMuonHits.initialise();
+    PixelLayersWithMeasurement.initialise();
+    NumberOfMatchedStations.initialise();
+    NumberOfMatches.initialise();
 }
 
 const MuonCollection& MuonReader::getMuons() {
@@ -61,10 +105,31 @@ void MuonReader::readMuons() {
     	double py = pyReader.getVariableAt(index);
     	double pz = pzReader.getVariableAt(index);
         MuonPointer muon(new Muon(energy, px, py, pz));
+        muon->setUsedAlgorithm(algorithm);
+        muon->setCharge(chargeReader.getIntVariableAt(index));
+
         muon->setEcalIsolation(ecalIsolationReader.getVariableAt(index));
         muon->setHcalIsolation(hcalIsolationReader.getVariableAt(index));
         muon->setTrackerIsolation(trackerIsolationReader.getVariableAt(index));
-        muon->makeGlobal(isGlobalReader.getBoolVariableAt(index));
+        muon->setPFGammaIsolation(PFGammaIsolationReader.getVariableAt(index));
+        muon->setPFChargedHadronIsolation(PFChargedHadronIsolationReader.getVariableAt(index));
+        muon->setPFNeutralHadronIsolation(PFNeutralHadronIsolationReader.getVariableAt(index));
+
+        muon->setD0(d0_PV_Reader.getVariableAt(index));
+        muon->setD0_wrtBeamSpot(d0_BS_Reader.getVariableAt(index));
+
+        muon->makeGlobal(isGlobalMuonReader.getBoolVariableAt(index));
+        muon->setTrackerMuon(isTrackerMuonReader.getBoolVariableAt(index));
+
+        muon->setZDistanceToPrimaryVertex(vertex_dist_z.getVariableAt(index));
+        muon->setNormalisedChi2(normalisedChi2.getVariableAt(index));
+        muon->setNumberOfValidHits(NumberOfValidHits.getIntVariableAt(index));
+        muon->setNumberOfValidMuonHits(NumberOfValidMuonHits.getIntVariableAt(index));
+        muon->setPixelLayersWithMeasurement(PixelLayersWithMeasurement.getIntVariableAt(index));
+        muon->setNumberOfMatchedStations(NumberOfMatchedStations.getIntVariableAt(index));
+        muon->setNumberOfMatches(NumberOfMatches.getIntVariableAt(index));
+
+
         muons.push_back(muon);
     }
 }
