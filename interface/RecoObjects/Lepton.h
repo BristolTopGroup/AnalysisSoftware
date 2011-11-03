607c634ffb3bc3ad836b61943c89a8d7f79d9edf
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-11-03 12:19:12 +0000

    added lepton class to unifiy the use of muons and electrons

---
+++ interface/RecoObjects/Lepton.h
@@ -0,0 +1,56 @@
+/*
+ * Lepton.h
+ *
+ *  Created on: 30 Oct 2011
+ *      Author: kreczko
+ */
+
+#ifndef LEPTON_H_
+#define LEPTON_H_
+
+#include "Particle.h"
+#include <boost/ptr_container/ptr_vector.hpp>
+namespace BAT {
+
+class Lepton: public BAT::Particle {
+public:
+	Lepton();
+	Lepton(double energy, double px, double py, double pz);
+	virtual ~Lepton() = 0;
+
+	virtual bool isGood(short leptonID) const = 0;
+	virtual bool isLoose() const = 0;
+
+	virtual bool isPFLepton() const = 0;
+
+	virtual void setEcalIsolation(double isolation);
+	virtual void setHcalIsolation(double isolation);
+	virtual void setTrackerIsolation(double isolation);
+	virtual double ecalIsolation() const;
+	virtual double hcalIsolation() const;
+	virtual double trackerIsolation() const;
+
+	virtual void setPFGammaIsolation(double pfGammaIso);
+	virtual void setPFChargedHadronIsolation(double chargedHadronIso);
+	virtual void setPFNeutralHadronIsolation(double neutralHadronIso);
+	virtual double PFGammaIsolation() const;
+	virtual double PFChargedHadronIsolation() const;
+	virtual double PFNeutralHadronIsolation() const;
+
+	virtual double relativeIsolation() const;
+	virtual double pfIsolation() const;
+
+	virtual void setZDistanceToPrimaryVertex(double dist);
+
+protected:
+	double ecal_Isolation, hcal_Isolation, tracker_Isolation;
+	double PFGamma_Isolation, PFChargedHadron_Isolation, PFNeutralHadron_Isolation;
+	double zDistanceToPrimaryVertex;
+
+};
+
+typedef boost::shared_ptr<Lepton> LeptonPointer;
+typedef boost::ptr_vector<Lepton> LeptonCollection;
+
+} /* namespace BAT */
+#endif /* LEPTON_H_ */
---
+++ src/RecoObjects/Lepton.cpp
@@ -0,0 +1,99 @@
+/*
+ * Lepton.cpp
+ *
+ *  Created on: 30 Oct 2011
+ *      Author: kreczko
+ */
+
+#include "../../interface/RecoObjects/Lepton.h"
+
+namespace BAT {
+const double initialBigValue = 123456789;
+
+Lepton::Lepton():
+		Particle(),
+		ecal_Isolation(initialBigValue),
+		hcal_Isolation(initialBigValue),
+		tracker_Isolation(initialBigValue),
+		PFGamma_Isolation(initialBigValue),
+		PFChargedHadron_Isolation(initialBigValue),
+		PFNeutralHadron_Isolation(initialBigValue),
+		zDistanceToPrimaryVertex(initialBigValue) {
+}
+
+Lepton::Lepton(double energy, double px, double py, double pz):
+		Particle(energy, px, py, pz),
+		ecal_Isolation(initialBigValue),
+		hcal_Isolation(initialBigValue),
+		tracker_Isolation(initialBigValue),
+		PFGamma_Isolation(initialBigValue),
+		PFChargedHadron_Isolation(initialBigValue),
+		PFNeutralHadron_Isolation(initialBigValue),
+		zDistanceToPrimaryVertex(initialBigValue) {
+
+}
+
+void Lepton::setEcalIsolation(double isolation) {
+	ecal_Isolation = isolation;
+}
+
+void Lepton::setHcalIsolation(double isolation) {
+	hcal_Isolation = isolation;
+}
+
+void Lepton::setTrackerIsolation(double isolation) {
+	tracker_Isolation = isolation;
+}
+
+double Lepton::trackerIsolation() const {
+	return tracker_Isolation;
+}
+
+double Lepton::hcalIsolation() const {
+	return hcal_Isolation;
+}
+
+double Lepton::ecalIsolation() const {
+	return ecal_Isolation;
+}
+
+void Lepton::setPFGammaIsolation(double pfGammaIso) {
+	PFGamma_Isolation = pfGammaIso;
+}
+
+void Lepton::setPFChargedHadronIsolation(double chargedHadronIso) {
+	PFChargedHadron_Isolation = chargedHadronIso;
+}
+
+void Lepton::setPFNeutralHadronIsolation(double neutralHadronIso) {
+	PFNeutralHadron_Isolation = neutralHadronIso;
+}
+
+double Lepton::PFGammaIsolation() const {
+	return PFGamma_Isolation;
+}
+
+double Lepton::PFChargedHadronIsolation() const {
+	return PFChargedHadron_Isolation;
+}
+
+double Lepton::PFNeutralHadronIsolation() const {
+	return PFNeutralHadron_Isolation;
+}
+
+double Lepton::relativeIsolation() const {
+	return (ecal_Isolation + hcal_Isolation + tracker_Isolation) / pt();
+}
+
+double Lepton::pfIsolation() const {
+	return (PFChargedHadron_Isolation + PFNeutralHadron_Isolation + PFGamma_Isolation) / pt();
+}
+
+void Lepton::setZDistanceToPrimaryVertex(double dist) {
+	zDistanceToPrimaryVertex = dist;
+}
+
+Lepton::~Lepton() {
+}
+
+} /* namespace BAT */
