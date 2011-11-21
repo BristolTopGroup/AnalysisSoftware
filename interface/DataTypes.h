ac5aa6c0cd24c63a1c9ae009c5cac3f13b6ea8f0
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-11-21 15:55:30 +0000

    fixed numbering

--- interface/DataTypes.h
+++ interface/DataTypes.h
@@ -22,8 +22,8 @@ enum value {
     QCD_BCtoE_Pt30to80,
     QCD_BCtoE_Pt80to170,
     QCD_EMEnriched_Pt20to30,
-    QCD_EMEnriched_Pt30to80,
-    QCD_EMEnriched_Pt80to170,//10
+    QCD_EMEnriched_Pt30to80, //10
+    QCD_EMEnriched_Pt80to170,
     PhotonJets_Pt40to100,
     PhotonJets_Pt100to200,
     PhotonJets_Pt200toInf,
@@ -32,27 +32,27 @@ enum value {
     singleTopTChannel,
     singleTopSChannel,
     singleAntiTop_And_W,
-	singleAntiTopTChannel,
+	singleAntiTopTChannel,//20
 	singleAntiTopSChannel,
     Zprime_M500GeV_W5GeV,
     Zprime_M500GeV_W50GeV,
-    Zprime_M750GeV_W7500MeV,//20
+    Zprime_M750GeV_W7500MeV,
     Zprime_M1TeV_W10GeV,
     Zprime_M1TeV_W100GeV,
     Zprime_M1250GeV_W12500MeV,
     Zprime_M1500GeV_W15GeV,
     Zprime_M1500GeV_W150GeV,
-    Zprime_M2TeV_W20GeV,
+    Zprime_M2TeV_W20GeV,//30
     Zprime_M2TeV_W200GeV,
     Zprime_M3TeV_W30GeV,
     Zprime_M3TeV_W300GeV,
-    Zprime_M4TeV_W40GeV,//30
+    Zprime_M4TeV_W40GeV,
     Zprime_M4TeV_W400GeV,
     WWtoAnything,
     WZtoAnything,
     ZZtoAnything,
     ttbar161,
-    ttbar163,
+    ttbar163,//40
     ttbar166,
     ttbar169,
     ttbar175,
