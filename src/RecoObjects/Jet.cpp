d99940bb16712941327e99edce0b2eb13a4da722
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-12-06 12:20:06 +0000

    - added following btags:    * Combined Secondary Vertex    * Combined Secondary Vertex MVA    * JetBProbability    * JetProbability    * Simple Secondary Vertex High Purity    * Soft Electron By IP3D    * Soft Electron By Pt    * Soft Muon    * Soft Muon By IP3D    * Soft Muon By Pt - added cuts for different working points for the above b-tag algorithms - renamed SimpleSecondaryVertexHighEff to SimpleSecondaryVertexHighEfficiency - added jet parton flavour

--- src/RecoObjects/Jet.cpp
+++ src/RecoObjects/Jet.cpp
@@ -11,39 +11,41 @@
 namespace BAT {
 
 Jet::Jet() :
-    Particle(),
-    usedAlgorithm(JetAlgorithm::Calo_AntiKT_Cone05),
-    electromagneticFraction(0.),
-    numberOfRecHitsContaining90PercentOfTheJetEnergy(0.),
-    fractionOfEnergyIntheHottestHPDReadout(0.),
-    btag_discriminators(BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS),
-    numberOfDaughters(0),
-    chargedEmEnergyFraction(1),
-    neutralHadronEnergyFraction(1),
-    neutralEmEnergyFraction(1),
-    chargedHadronEnergyFraction(1),
-    chargedMultiplicity(0),
-    partonFlavour(0) {
-    for (unsigned int btag = 0; btag < btag_discriminators.size(); ++btag) {
-        btag_discriminators[btag] = -9999;
-    }
+		Particle(), //
+		usedAlgorithm(JetAlgorithm::Calo_AntiKT_Cone05), //
+		electromagneticFraction(0.), //
+		numberOfRecHitsContaining90PercentOfTheJetEnergy(0.), //
+		fractionOfEnergyIntheHottestHPDReadout(0.), //
+		btag_discriminators(BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS), //
+		numberOfDaughters(0), //
+		chargedEmEnergyFraction(1), //
+		neutralHadronEnergyFraction(1), //
+		neutralEmEnergyFraction(1), //
+		chargedHadronEnergyFraction(1), //
+		chargedMultiplicity(0), //
+		partonFlavour_(0) //
+{
+	for (unsigned int btag = 0; btag < btag_discriminators.size(); ++btag) {
+		btag_discriminators[btag] = -9999;
+	}
 }
 
 Jet::Jet(double energy, double px, double py, double pz) :
-    Particle(energy, px, py, pz),
-    usedAlgorithm(JetAlgorithm::Calo_AntiKT_Cone05),
-    electromagneticFraction(0.),
-    btag_discriminators(BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS),
-    numberOfDaughters(0),
-    chargedEmEnergyFraction(1),
-    neutralHadronEnergyFraction(1),
-    neutralEmEnergyFraction(1),
-    chargedHadronEnergyFraction(1),
-    chargedMultiplicity(0),
-    partonFlavour(0) {
-    for (unsigned int btag = 0; btag < btag_discriminators.size(); ++btag) {
-        btag_discriminators[btag] = -9999;
-    }
+		Particle(energy, px, py, pz), //
+		usedAlgorithm(JetAlgorithm::Calo_AntiKT_Cone05), //
+		electromagneticFraction(0.), //
+		btag_discriminators(BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS), //
+		numberOfDaughters(0), //
+		chargedEmEnergyFraction(1), //
+		neutralHadronEnergyFraction(1), //
+		neutralEmEnergyFraction(1), //
+		chargedHadronEnergyFraction(1), //
+		chargedMultiplicity(0), //
+		partonFlavour_(0) //
+{
+	for (unsigned int btag = 0; btag < btag_discriminators.size(); ++btag) {
+		btag_discriminators[btag] = -9999;
+	}
 
 }
 
@@ -51,93 +53,97 @@ Jet::~Jet() {
 }
 
 JetAlgorithm::value Jet::getUsedAlgorithm() const {
-    return usedAlgorithm;
+	return usedAlgorithm;
 }
 
 double Jet::emf() const {
-    return electromagneticFraction;
+	return electromagneticFraction;
 }
 
 double Jet::n90Hits() const {
-    return numberOfRecHitsContaining90PercentOfTheJetEnergy;
+	return numberOfRecHitsContaining90PercentOfTheJetEnergy;
 }
 
 double Jet::fHPD() const {
-    return fractionOfEnergyIntheHottestHPDReadout;
+	return fractionOfEnergyIntheHottestHPDReadout;
 }
 
 double Jet::NOD() const {
-    return numberOfDaughters;
+	return numberOfDaughters;
 }
 
 double Jet::CEF() const {
-    return chargedEmEnergyFraction;
+	return chargedEmEnergyFraction;
 }
 
 double Jet::NHF() const {
-    return neutralHadronEnergyFraction;
+	return neutralHadronEnergyFraction;
 }
 
 double Jet::NEF() const {
-    return neutralEmEnergyFraction;
+	return neutralEmEnergyFraction;
 }
 
 double Jet::CHF() const {
-    return chargedHadronEnergyFraction;
+	return chargedHadronEnergyFraction;
 }
 
 double Jet::NCH() const {
-    return chargedMultiplicity;
+	return chargedMultiplicity;
 }
 
 void Jet::setUsedAlgorithm(JetAlgorithm::value algo) {
-    usedAlgorithm = algo;
+	usedAlgorithm = algo;
 }
 void Jet::setEMF(double emf) {
-    electromagneticFraction = emf;
+	electromagneticFraction = emf;
 }
 
 void Jet::setN90Hits(int n90Hits) {
-    numberOfRecHitsContaining90PercentOfTheJetEnergy = n90Hits;
+	numberOfRecHitsContaining90PercentOfTheJetEnergy = n90Hits;
 }
 
 void Jet::setFHPD(double fHPD) {
-    fractionOfEnergyIntheHottestHPDReadout = fHPD;
+	fractionOfEnergyIntheHottestHPDReadout = fHPD;
 }
 
 void Jet::setDiscriminatorForBtagType(double discriminator, BtagAlgorithm::value type) {
-    btag_discriminators[type] = discriminator;
+	btag_discriminators[type] = discriminator;
 }
 
 void Jet::setNOD(int nod) {
-    numberOfDaughters = nod;
+	numberOfDaughters = nod;
 }
 void Jet::setCEF(double cef) {
-    chargedEmEnergyFraction = cef;
+	chargedEmEnergyFraction = cef;
 }
 void Jet::setNHF(double nhf) {
-    neutralHadronEnergyFraction = nhf;
+	neutralHadronEnergyFraction = nhf;
 }
 void Jet::setNEF(double nef) {
-    neutralEmEnergyFraction = nef;
+	neutralEmEnergyFraction = nef;
 }
 void Jet::setCHF(double chf) {
-    chargedHadronEnergyFraction = chf;
+	chargedHadronEnergyFraction = chf;
 }
 void Jet::setNCH(double nch) {
-    chargedMultiplicity = nch;
+	chargedMultiplicity = nch;
+}
+
+void Jet::setPartonFlavour(int flavour) {
+	partonFlavour_ = flavour;
 }
 
 bool Jet::isGood() const {
-    bool passesPt = pt() > 30;
-    bool passesEta = fabs(eta()) < 2.4;
-    bool jetID = false;
-    if (usedAlgorithm == JetAlgorithm::CA08PF || usedAlgorithm == JetAlgorithm::PF2PAT)
-        jetID = FirstDataLoosePFJetID();
-    else
-        jetID = FirstDataLooseCaloJetID();
+	bool passesPt = pt() > 30;
+	bool passesEta = fabs(eta()) < 2.4;
+	bool jetID = false;
+	if (usedAlgorithm == JetAlgorithm::CA08PF || usedAlgorithm == JetAlgorithm::PF2PAT)
+		jetID = FirstDataLoosePFJetID();
+	else
+		jetID = FirstDataLooseCaloJetID();
 
-    return passesPt && passesEta && jetID;
+	return passesPt && passesEta && jetID;
 }
 
 bool Jet::FirstDataLoosePFJetID() const {
@@ -164,95 +170,138 @@ bool Jet::FirstDataLooseCaloJetID() const {
 /*
  * Values taken from
  * https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideBTagPerformance
+ * The b-tag POG has defined working points "Loose " (L), "Medium" (M) and "Tight" (T) for 10%, 1% and 0.1% light mis tag :
+ * more details on these working points can be found on https://twiki.cern.ch/twiki/bin/viewauth/CMS/BTagPerformanceOP
  */
 bool Jet::isBJet(BtagAlgorithm::value type, BtagAlgorithm::workingPoint wp) const {
-    double cut(9998);
-    switch (type) {
-    case BtagAlgorithm::GenPartonFlavour:
-        return abs(partonFlavour) == 5;
+	double cut(9998);
+	switch (type) {
 
-    case BtagAlgorithm::SimpleSecondaryVertexHighEffBTag:
-        if (wp == BtagAlgorithm::LOOSE)
-            cut = 9998;//no input found
-        else if (wp == BtagAlgorithm::MEDIUM)
-            cut = 1.74;
-        else if (wp == BtagAlgorithm::TIGHT)
-            cut = 3.05;
-        break;
+	case BtagAlgorithm::CombinedSecondaryVertex:
+		switch (wp) {
+		case BtagAlgorithm::LOOSE:
+			cut = 0.244;
+			break;
+		case BtagAlgorithm::MEDIUM:
+			cut = 0.679;
+			break;
+		case BtagAlgorithm::TIGHT:
+			cut = 0.898;
+			break;
+		}
 
-    case BtagAlgorithm::SimpleSecondaryVertexHighPurBTag:
-        if (wp == BtagAlgorithm::LOOSE)
-            cut = 9998;//no input found
-        else if (wp == BtagAlgorithm::MEDIUM)
-            cut = 9998;//no input found
-        else if (wp == BtagAlgorithm::TIGHT)
-            cut = 2.;
-        break;
+	case BtagAlgorithm::CombinedSecondaryVertexMVA:
+		//same as without MVA? No hints found
+		//according to
+		//https://cms-btag-validation.web.cern.ch/cms-btag-validation/validation/index_RecoB_CMSSW_4_2_1_TTbar_421_vs_420_Startup.html
+		//these cuts should be lower
+		switch (wp) {
+		case BtagAlgorithm::LOOSE:
+			cut = 0.244;
+			break;
+		case BtagAlgorithm::MEDIUM:
+			cut = 0.679;
+			break;
+		case BtagAlgorithm::TIGHT:
+			cut = 0.898;
+			break;
+		}
 
-    case BtagAlgorithm::TrackCountingHighEffBTag:
-        if (wp == BtagAlgorithm::LOOSE)
-            cut = 1.7;
-        else if (wp == BtagAlgorithm::MEDIUM)
-            cut = 3.3;
-        else if (wp == BtagAlgorithm::TIGHT)
-            cut = 10.2;
-        break;
+	case BtagAlgorithm::JetBProbability:
+		switch (wp) {
+		case BtagAlgorithm::LOOSE:
+			cut = 1.33;
+			break;
+		case BtagAlgorithm::MEDIUM:
+			cut = 2.55;
+			break;
+		case BtagAlgorithm::TIGHT:
+			cut = 3.74;
+			break;
+		}
 
-    case BtagAlgorithm::TrackCountingHighPurBTag:
-        if (wp == BtagAlgorithm::LOOSE)
-            cut = 1.19;
-        else if (wp == BtagAlgorithm::MEDIUM)
-            cut = 1.93;
-        else if (wp == BtagAlgorithm::TIGHT)
-            cut = 3.41;
-        break;
+	case BtagAlgorithm::JetProbability:
+		switch (wp) {
+		case BtagAlgorithm::LOOSE:
+			cut = 0.275;
+			break;
+		case BtagAlgorithm::MEDIUM:
+			cut = 0.545;
+			break;
+		case BtagAlgorithm::TIGHT:
+			cut = 0.790;
+			break;
+		}
 
-    case BtagAlgorithm::JetProbabilityBTag:
-        if (wp == BtagAlgorithm::LOOSE)
-            cut = 0.215;
-        else if (wp == BtagAlgorithm::MEDIUM)
-            cut = 0.459;
-        else if (wp == BtagAlgorithm::TIGHT)
-            cut = 0.669;
-        break;
+	case BtagAlgorithm::SimpleSecondaryVertexHighEfficiency:
+		if (wp == BtagAlgorithm::LOOSE)
+			cut = 9998; //no input found
+		else if (wp == BtagAlgorithm::MEDIUM)
+			cut = 1.74;
+		else if (wp == BtagAlgorithm::TIGHT)//not supported
+			cut = 3.05;
+		break;
 
-    case BtagAlgorithm::JetBProbabilityBTag:
-        if (wp == BtagAlgorithm::LOOSE)
-            cut = 0.988;
-        else if (wp == BtagAlgorithm::MEDIUM)
-            cut = 1.83;
-        else if (wp == BtagAlgorithm::TIGHT)
-            cut = 1.95;
-        break;
-    default:
-        return false;
+	case BtagAlgorithm::SimpleSecondaryVertexHighPurity:
+		if (wp == BtagAlgorithm::LOOSE)
+			cut = 9998; //no input found
+		else if (wp == BtagAlgorithm::MEDIUM)
+			cut = 9998; //no input found
+		else if (wp == BtagAlgorithm::TIGHT)
+			cut = 2.;
+		break;
 
-    }
-    return btag_discriminators[type] > cut;
+	case BtagAlgorithm::TrackCountingHighEfficiency:
+		if (wp == BtagAlgorithm::LOOSE)
+			cut = 1.7;
+		else if (wp == BtagAlgorithm::MEDIUM)
+			cut = 3.3;
+		else if (wp == BtagAlgorithm::TIGHT)//not supported
+			cut = 10.2;
+		break;
+
+	case BtagAlgorithm::TrackCountingHighPurity:
+		if (wp == BtagAlgorithm::LOOSE)//not supported
+			cut = 1.19;
+		else if (wp == BtagAlgorithm::MEDIUM)
+			cut = 1.93;
+		else if (wp == BtagAlgorithm::TIGHT)
+			cut = 3.41;
+		break;
+
+	default:
+		return false;
+
+	}
+	return btag_discriminators[type] > cut;
 }
 
 double Jet::btagSSVHE() const {
-	return btag_discriminators[BtagAlgorithm::SimpleSecondaryVertexHighEffBTag];
+	return btag_discriminators[BtagAlgorithm::SimpleSecondaryVertexHighEfficiency];
 }
 
 double Jet::getBJetL7EtCorrection() const {
-    Float_t towerBinning[] = {0.0, 0.174, 0.348, 0.522, 0.696, 0.87, 1.044, 1.218, 1.392, 1.566, 1.74, 2.5};
-    unsigned int nEtaBins_ = 11;
-    double res = -1;
-    for(UInt_t iEta = 0; iEta < nEtaBins_; iEta++)
-    if(fabs(eta()) >= towerBinning[iEta] && fabs(eta()) < towerBinning[iEta+1])
-        res = Globals::bL7Corrections[iEta]->Eval( pt() );
-    return res;
+	Float_t towerBinning[] = { 0.0, 0.174, 0.348, 0.522, 0.696, 0.87, 1.044, 1.218, 1.392, 1.566, 1.74, 2.5 };
+	unsigned int nEtaBins_ = 11;
+	double res = -1;
+	for (UInt_t iEta = 0; iEta < nEtaBins_; iEta++)
+		if (fabs(eta()) >= towerBinning[iEta] && fabs(eta()) < towerBinning[iEta + 1])
+			res = Globals::bL7Corrections[iEta]->Eval(pt());
+	return res;
 }
 
 double Jet::getLightJetL7EtCorrection() const {
-    Float_t towerBinning[] = {0.0, 0.174, 0.348, 0.522, 0.696, 0.87, 1.044, 1.218, 1.392, 1.566, 1.74, 2.5};
-    unsigned int nEtaBins_ = 11;
-    double res = -1;
-    for(UInt_t iEta = 0; iEta < nEtaBins_; iEta++)
-      if(fabs(eta()) >= towerBinning[iEta] && fabs(eta()) < towerBinning[iEta+1])
-        res = Globals::lightL7Corrections[iEta]->Eval( pt() );
-    return res;
+	Float_t towerBinning[] = { 0.0, 0.174, 0.348, 0.522, 0.696, 0.87, 1.044, 1.218, 1.392, 1.566, 1.74, 2.5 };
+	unsigned int nEtaBins_ = 11;
+	double res = -1;
+	for (UInt_t iEta = 0; iEta < nEtaBins_; iEta++)
+		if (fabs(eta()) >= towerBinning[iEta] && fabs(eta()) < towerBinning[iEta + 1])
+			res = Globals::lightL7Corrections[iEta]->Eval(pt());
+	return res;
+}
+
+int Jet::partonFlavour() const {
+	return partonFlavour_;
 }
 
 }
