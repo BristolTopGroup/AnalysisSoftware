9e9b28979ed44943c1df8c280a14e206ad48e912
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2012-03-14 16:00:00 +0000

    initial import of skeleton for a Protocol Buffer Reader

---
+++ interface/Readers/ProtoBufEventReader.h
@@ -0,0 +1,20 @@
+/*
+ * ProtoBufEventReader.h
+ *
+ *  Created on: 18 Sep 2011
+ *      Author: kreczko
+ */
+
+#ifndef PROTOBUFEVENTREADER_H_
+#define PROTOBUFEVENTREADER_H_
+
+namespace BAT {
+
+class ProtoBufEventReader {
+public:
+	ProtoBufEventReader();
+	virtual ~ProtoBufEventReader();
+};
+
+} /* namespace BAT */
+#endif /* PROTOBUFEVENTREADER_H_ */
---
+++ src/Readers/ProtoBufEventReader.cpp
@@ -0,0 +1,19 @@
+/*
+ * ProtoBufEventReader.cpp
+ *
+ *  Created on: 18 Sep 2011
+ *      Author: kreczko
+ */
+
+#include "../../interface/Readers/ProtoBufEventReader.h"
+
+namespace BAT {
+
+ProtoBufEventReader::ProtoBufEventReader() {
+
+}
+
+ProtoBufEventReader::~ProtoBufEventReader() {
+}
+
+} /* namespace BAT */
