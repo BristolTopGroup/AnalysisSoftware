58af336abe909c125d4b51b353192b4871fd1b04
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2012-03-26 19:49:21 +0000

    added MET significance vs. MET plots

--- src/Analysers/TTbarPlusMETAnalyser.cpp
+++ src/Analysers/TTbarPlusMETAnalyser.cpp
@@ -14,24 +14,29 @@ void TTbarPlusMETAnalyser::analyse(const EventPtr event) {
 	histMan->setCurrentHistogramFolder("TTbarPlusMetAnalysis");
 	TopPairEventCandidatePtr ttbarCand(new TopPairEventCandidate(*event.get()));
 	double weight = event->weight();
+	const METPointer met = event->MET();
 
 	if (ttbarCand->passesFullTTbarEPlusJetSelection()) {
-		histMan->H1D_BJetBinned("MET")->Fill(event->MET()->et(), weight);
+		histMan->H1D_BJetBinned("MET")->Fill(met->et(), weight);
+		histMan->H2D_BJetBinned("METsignificance_vs_MET")->Fill(met->et(),met->significance(), weight);
 		if (ttbarCand->passesAsymmetricElectronCleanedJetCuts()) {
-			histMan->H1D_BJetBinned("MET_withAsymJetsCut")->Fill(event->MET()->et(), weight);
+			histMan->H1D_BJetBinned("MET_withAsymJetsCut")->Fill(met->et(), weight);
+			histMan->H2D_BJetBinned("METsignificance_vs_MET_withAsymJetsCut")->Fill(met->et(), met->significance(), weight);
 			if (ttbarCand->passesMETCut()) {
-				histMan->H1D_BJetBinned("MET_withMETAndAsymJets")->Fill(event->MET()->et(), weight);
+				histMan->H1D_BJetBinned("MET_withMETAndAsymJets")->Fill(met->et(), weight);
 			}
 		}
 	}
 
 	if (ttbarCand->passesEPlusJetsSelectionStepUpTo(TTbarEPlusJetsSelection::AtLeastThreeGoodJets)
 			&& ttbarCand->GoodElectronCleanedJets().size() == 3) {
-		histMan->H1D_BJetBinned("MET_3jets")->Fill(event->MET()->et(), weight);
+		histMan->H1D_BJetBinned("MET_3jets")->Fill(met->et(), weight);
+		histMan->H2D_BJetBinned("METsignificance_vs_MET_3jets")->Fill(met->et(), met->significance(), weight);
 		if (ttbarCand->passesAsymmetricElectronCleanedJetCuts()) {
-			histMan->H1D_BJetBinned("MET_withAsymJetsCut_3jets")->Fill(event->MET()->et(), weight);
+			histMan->H1D_BJetBinned("MET_withAsymJetsCut_3jets")->Fill(met->et(), weight);
+			histMan->H2D_BJetBinned("METsignificance_vs_MET_withAsymJetsCut_3jets")->Fill(met->et(), met->significance(), weight);
 			if (ttbarCand->passesMETCut()) {
-				histMan->H1D_BJetBinned("MET_withMETAndAsymJets_3jets")->Fill(event->MET()->et(), weight);
+				histMan->H1D_BJetBinned("MET_withMETAndAsymJets_3jets")->Fill(met->et(), weight);
 			}
 		}
 	}
@@ -46,6 +51,16 @@ void TTbarPlusMETAnalyser::createHistograms() {
 	histMan->addH1D_BJetBinned("MET_3jets", "MET", 1000, 0, 1000);
 	histMan->addH1D_BJetBinned("MET_withMETAndAsymJets_3jets", "MET", 1000, 0, 1000);
 	histMan->addH1D_BJetBinned("MET_withAsymJetsCut_3jets", "MET", 1000, 0, 1000);
+
+	histMan->addH2D_BJetBinned("METsignificance_vs_MET", "MET vs MET significance;MET; MET significance", 1000, 0, 1000,
+			200, 0, 1000);
+	histMan->addH2D_BJetBinned("METsignificance_vs_MET_withAsymJetsCut", "MET vs MET significance;MET; MET significance", 1000, 0, 1000,
+			200, 0, 1000);
+
+	histMan->addH2D_BJetBinned("METsignificance_vs_MET_3jets", "MET vs MET significance;MET; MET significance", 1000, 0, 1000,
+			200, 0, 1000);
+	histMan->addH2D_BJetBinned("METsignificance_vs_MET_withAsymJetsCut_3jets", "MET vs MET significance;MET; MET significance", 1000, 0, 1000,
+			200, 0, 1000);
 }
 
 TTbarPlusMETAnalyser::TTbarPlusMETAnalyser(HistogramManagerPtr histMan) :
