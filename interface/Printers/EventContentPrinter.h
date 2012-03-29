85799575a0275309685cfa41536c632619b5cd43
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2012-03-29 15:39:50 +0000

    added print method for muons and extended electron information. This will change in the near future as each RecoObject will get its own toString() function (Java style)

--- interface/Printers/EventContentPrinter.h
+++ interface/Printers/EventContentPrinter.h
@@ -11,16 +11,19 @@
 #include "../RecoObjects/Particle.h"
 #include "../RecoObjects/Electron.h"
 #include "../RecoObjects/Jet.h"
+#include "../RecoObjects/Muon.h"
 
 namespace BAT {
 
 namespace EventContentPrinter {
     extern void printJets(const JetCollection jets);
     extern void printElectrons(const ElectronCollection electrons);
+    extern void printMuons(const MuonCollection muons);
 
     extern void printJet(const JetPointer jet);
     extern void printPFJetExtra(const JetPointer jet);
     extern void printElectron(const ElectronPointer electron);
+    extern void printMuon(const MuonPointer muon);
     extern void printParticle(const ParticlePointer particle);
     extern void printGenParticle(const MCParticlePointer genParticle);
     extern void printCutFlowTable();
