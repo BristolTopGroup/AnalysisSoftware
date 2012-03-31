0809be23694ea0ccd270df15820163cd88f8e0d8
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2012-03-31 13:36:51 +0000

    - first full upload of Jesons BJet analyser - set BTag names in Jet.h for easier access to related histograms - shortened code: automated histogram creation and filling

--- interface/RecoObjects/Jet.h
+++ interface/RecoObjects/Jet.h
@@ -19,108 +19,118 @@ namespace BtagAlgorithm {
 enum value {
 	CombinedSecondaryVertex,
 	CombinedSecondaryVertexMVA,
-    JetBProbability,
-    JetProbability,
-    SimpleSecondaryVertexHighEfficiency,
-    SimpleSecondaryVertexHighPurity,
-    SoftElectronByIP3d,
-    SoftElectronByPt,
-    SoftMuon,
-    SoftMuonByIP3d,
-    SoftMuonByPt,
-    TrackCountingHighEfficiency,
-    TrackCountingHighPurity,
-    NUMBER_OF_BTAGALGORITHMS
+	JetBProbability,
+	JetProbability,
+	SimpleSecondaryVertexHighEfficiency,
+	SimpleSecondaryVertexHighPurity,
+	SoftElectronByIP3d,
+	SoftElectronByPt,
+	SoftMuon,
+	SoftMuonByIP3d,
+	SoftMuonByPt,
+	TrackCountingHighEfficiency,
+	TrackCountingHighPurity,
+	NUMBER_OF_BTAGALGORITHMS
 };
 
 enum workingPoint {
-    LOOSE, MEDIUM, TIGHT
+	LOOSE, MEDIUM, TIGHT
 };
+const boost::array<std::string, BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS> names = { {
+		"CombinedSecondaryVertex",//
+		"CombinedSecondaryVertexMVA",//
+		"JetBProbability",//
+		"JetProbability",//
+		"SimpleSecondaryVertexHighEfficiency",//
+		"SimpleSecondaryVertexHighPurity",//
+		"SoftElectronByIP3d",//
+		"SoftElectronByPt",//
+		"SoftMuon",//
+		"SoftMuonByIP3d",//
+		"SoftMuonByPt",//
+		"TrackCountingHighEfficiency",//
+		"TrackCountingHighPurity"
+	}}
+	;
+
 }
 
 namespace JetAlgorithm {
 enum value {
-    Calo_AntiKT_Cone05,
-    CA08PF,
-    PF2PAT,
-    NUMBER_OF_JETALGORITHMS
+	Calo_AntiKT_Cone05, CA08PF, PF2PAT, NUMBER_OF_JETALGORITHMS
 };
 //for nTuple branch names
-const boost::array<std::string, JetAlgorithm::NUMBER_OF_JETALGORITHMS> prefixes = { {
-        "goodPatJets",
-        "goodPatJetsCA8PF",
-        "goodPatJetsPFlow" } };
+const boost::array<std::string, JetAlgorithm::NUMBER_OF_JETALGORITHMS> prefixes = { { "goodPatJets", "goodPatJetsCA8PF",
+		"goodPatJetsPFlow" } };
 
-const boost::array<std::string, JetAlgorithm::NUMBER_OF_JETALGORITHMS> names = { {
-        "CaloJets",
-        "CA08PFJets",
-        "PF2PATJets" } };
+const boost::array<std::string, JetAlgorithm::NUMBER_OF_JETALGORITHMS> names = { { "CaloJets", "CA08PFJets",
+		"PF2PATJets" } };
 }
 
 class Jet: public Particle {
 public:
 
-    Jet();
-    Jet(const Particle& particle);
-    Jet(double energy, double px, double py, double pz);
-    virtual ~Jet();
-    bool isGood() const;
-    bool isBJet(BtagAlgorithm::value type = BtagAlgorithm::SimpleSecondaryVertexHighEfficiency,
-            BtagAlgorithm::workingPoint wp = BtagAlgorithm::MEDIUM) const;
-    JetAlgorithm::value getUsedAlgorithm() const;
-    double emf() const;
-    double n90Hits() const;
-    double fHPD() const;
-    double NOD() const;
-    double CEF() const;
-    double NHF() const;
-    double NEF() const;
-    double CHF() const;
-    double NCH() const;
-    double btagSSVHE() const;
-    double getBJetL7EtCorrection() const;
-    double getLightJetL7EtCorrection() const;
-    //Quarks: d=1, u=2, s=3, c=4, b=5 (antimatter with flipped sign)
-    // gluons = 21
-    int partonFlavour() const;
-    double PxRaw() const;
-    double PyRaw() const;
-    double PzRaw() const;
-    double JECUnc() const;
-    double getBTagDiscriminator(BtagAlgorithm::value type) const;
+	Jet();
+	Jet(const Particle& particle);
+	Jet(double energy, double px, double py, double pz);
+	virtual ~Jet();
+	bool isGood() const;
+	bool isBJet(BtagAlgorithm::value type = BtagAlgorithm::SimpleSecondaryVertexHighEfficiency,
+			BtagAlgorithm::workingPoint wp = BtagAlgorithm::MEDIUM) const;
+	JetAlgorithm::value getUsedAlgorithm() const;
+	double emf() const;
+	double n90Hits() const;
+	double fHPD() const;
+	double NOD() const;
+	double CEF() const;
+	double NHF() const;
+	double NEF() const;
+	double CHF() const;
+	double NCH() const;
+	double btagSSVHE() const;
+	double getBJetL7EtCorrection() const;
+	double getLightJetL7EtCorrection() const;
+	//Quarks: d=1, u=2, s=3, c=4, b=5 (antimatter with flipped sign)
+	// gluons = 21
+	int partonFlavour() const;
+	double PxRaw() const;
+	double PyRaw() const;
+	double PzRaw() const;
+	double JECUnc() const;
+	double getBTagDiscriminator(BtagAlgorithm::value type) const;
 
-    void setPxRaw(double px);
-    void setPyRaw(double py);
-    void setPzRaw(double pz);
-    void setJECUnc(double JECUnc);
-    void setUsedAlgorithm(JetAlgorithm::value algo);
-    void setEMF(double emf);
-    void setN90Hits(int n90Hits);
-    void setFHPD(double fHPD);
-    void setDiscriminatorForBtagType(double discriminator, BtagAlgorithm::value type);
-    void setNOD(int nod);
-    void setCEF(double cef);
-    void setNHF(double nhf);
-    void setNEF(double nef);
-    void setCHF(double chf);
-    void setNCH(double nch);
+	void setPxRaw(double px);
+	void setPyRaw(double py);
+	void setPzRaw(double pz);
+	void setJECUnc(double JECUnc);
+	void setUsedAlgorithm(JetAlgorithm::value algo);
+	void setEMF(double emf);
+	void setN90Hits(int n90Hits);
+	void setFHPD(double fHPD);
+	void setDiscriminatorForBtagType(double discriminator, BtagAlgorithm::value type);
+	void setNOD(int nod);
+	void setCEF(double cef);
+	void setNHF(double nhf);
+	void setNEF(double nef);
+	void setCHF(double chf);
+	void setNCH(double nch);
 //    void setRho(double rho);
-    void setPartonFlavour(int);
+	void setPartonFlavour(int);
 
-    bool FirstDataLoosePFJetID() const;
-    bool FirstDataLooseCaloJetID() const;
+	bool FirstDataLoosePFJetID() const;
+	bool FirstDataLooseCaloJetID() const;
 private:
-    JetAlgorithm::value usedAlgorithm;
-    double electromagneticFraction;
-    int numberOfRecHitsContaining90PercentOfTheJetEnergy;
-    double fractionOfEnergyIntheHottestHPDReadout;
-    std::vector<double> btag_discriminators;
-    //	double btagInData;
-    int numberOfDaughters;
-    double chargedEmEnergyFraction, neutralHadronEnergyFraction, neutralEmEnergyFraction;
-    double chargedHadronEnergyFraction, chargedMultiplicity;
-    double pxRaw, pyRaw, pzRaw, JECUncertainty;
-    int partonFlavour_;
+	JetAlgorithm::value usedAlgorithm;
+	double electromagneticFraction;
+	int numberOfRecHitsContaining90PercentOfTheJetEnergy;
+	double fractionOfEnergyIntheHottestHPDReadout;
+	std::vector<double> btag_discriminators;
+	//	double btagInData;
+	int numberOfDaughters;
+	double chargedEmEnergyFraction, neutralHadronEnergyFraction, neutralEmEnergyFraction;
+	double chargedHadronEnergyFraction, chargedMultiplicity;
+	double pxRaw, pyRaw, pzRaw, JECUncertainty;
+	int partonFlavour_;
 };
 
 typedef boost::shared_ptr<Jet> JetPointer;
--- src/Analysers/BJetAnalyser.cpp
+++ src/Analysers/BJetAnalyser.cpp
@@ -2,41 +2,72 @@
  * BJetAnalyser.cpp
  *
  *  Created on: 3 Jul 2011
- *      Author: kreczko
+ *      Author: J. Jacob
+ *      Modified: L . Kreczko
  */
 #include "../../interface/Analysers/BJetAnalyser.h"
 #include "../../interface/GlobalVariables.h"
 
 namespace BAT {
 
-void BJetAnalyser::analyse(const EventPtr event) {
-	double weight =  event->weight();
-    histMan_->setCurrentHistogramFolder("BJetAnalysis");
-    const JetCollection allJets =  event->Jets();
+const boost::array<std::string, 5> JETTYPES = { { "allJets", //
+		"bQuarkJets", //
+		"cQuarkJets", //
+		"udsQuarkJets", //
+		"gluonJets" } };
 
-    for(unsigned int jetIndex = 0; jetIndex < allJets.size(); ++jetIndex){
-    	const JetPointer jet(allJets.at(jetIndex));
-    	double ssvHE_disc = jet->getBTagDiscriminator(BtagAlgorithm::SimpleSecondaryVertexHighEfficiency);
-    	histMan_->H1D("ssv_HE_disc")->Fill(ssvHE_disc, weight);
-    	if(abs(jet->partonFlavour()) == 5)
-    		histMan_->H1D("ssv_HE_disc_bjets")->Fill(ssvHE_disc, weight);
-    }
+void BJetAnalyser::analyse(const EventPtr event) {
+	double weight = event->weight();
+	histMan_->setCurrentHistogramFolder("BJetAnalysis");
+	const JetCollection allJets = event->Jets();
+
+	for (unsigned int jetIndex = 0; jetIndex < allJets.size(); ++jetIndex) {
+		const JetPointer jet(allJets.at(jetIndex));
+
+		for (unsigned int algo = 0; algo < BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS; ++algo) {
+			string algorithm = BtagAlgorithm::names.at(algo);
+			double disc = jet->getBTagDiscriminator((BtagAlgorithm::value) algo);
+
+			histMan_->H1D(algorithm + "_allJets")->Fill(disc, weight);
+
+			unsigned int partonFlavour = abs(jet->partonFlavour());
+			if (partonFlavour == 5) {
+				//partonFlavour 5 = b
+				histMan_->H1D(algorithm + "_bQuarkJets")->Fill(disc, weight);
+			} else if (partonFlavour == 4) {
+				//partonFlavour 4 = c
+				histMan_->H1D(algorithm + "_cQuarkJets")->Fill(disc, weight);
+			} else if (partonFlavour == 1 || partonFlavour == 2 || partonFlavour == 3) {
+				//partonFlavour 1 = d, 2 = u, 3 = s
+				histMan_->H1D(algorithm + "_udsQuarkJets")->Fill(disc, weight);
+			} else if (abs(jet->partonFlavour()) == 21) {
+				//partonFlavour 21 = gluons
+				histMan_->H1D(algorithm + "_gluonJets")->Fill(disc, weight);
+			}
+		}
+	}
+
 }
 
-
 void BJetAnalyser::createHistograms() {
-    histMan_->setCurrentHistogramFolder("BJetAnalysis");
+	histMan_->setCurrentHistogramFolder("BJetAnalysis");
 
-    histMan_->addH1D("ssv_HE_disc", "ssv_HE_disc", 1000, -1., 10.);
-    histMan_->addH1D("ssv_HE_disc_bjets", "ssv_HE_disc_bjets", 1000, -1., 10.);
-
+	for (unsigned int algo = 0; algo < BtagAlgorithm::NUMBER_OF_BTAGALGORITHMS; ++algo) {
+		for (unsigned int index = 0; index < JETTYPES.size(); ++index) {
+			string jettype = JETTYPES.at(index);
+			string algorithm = BtagAlgorithm::names.at(algo);
+			string histname = algorithm + "_" + jettype;
+			histMan_->addH1D(histname, histname, 1100, -1., 10.);
+		}
+	}
 }
 
 BJetAnalyser::BJetAnalyser(HistogramManagerPtr histMan, std::string histogramFolder) :
-    BasicAnalyser(histMan, histogramFolder) {
+		BasicAnalyser(histMan, histogramFolder) {
 
 }
 
 BJetAnalyser::~BJetAnalyser() {
 }
-}
+
+} //end namespace BAT
