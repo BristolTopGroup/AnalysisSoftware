0d9014483a064368682669ac31310fe4c8a62ff1
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2012-04-13 12:51:23 +0000

    added a more clear message for a missing variable.

--- interface/Readers/VariableReader.h
+++ interface/Readers/VariableReader.h
@@ -22,90 +22,92 @@ typedef std::vector<unsigned int>* MultiUIntPointer;
 typedef boost::shared_ptr<TChain> TChainPointer;
 namespace BAT {
 struct VariableNotFoundException: public std::exception {
-    TString msg;
-    VariableNotFoundException(TString message) :
-        msg(message) {
-    }
-    ~VariableNotFoundException() throw () {
-    }
+	TString msg;
+	VariableNotFoundException(TString message) :
+			msg(message) {
+	}
+	~VariableNotFoundException() throw () {
+	}
 
-    const char* what() const throw () {
-        return msg;
-    }
+	const char* what() const throw () {
+		return msg;
+	}
 };
 
 struct VariableNotInitialisedException: public std::exception {
-    TString msg;
-    VariableNotInitialisedException(TString message) :
-        msg(message) {
-    }
-    ~VariableNotInitialisedException() throw () {
-    }
+	TString msg;
+	VariableNotInitialisedException(TString message) :
+			msg(message) {
+	}
+	~VariableNotInitialisedException() throw () {
+	}
 
-    const char* what() const throw () {
-        return msg;
-    }
+	const char* what() const throw () {
+		return msg;
+	}
 };
 
 template<typename variableType = unsigned int>
 class VariableReader {
 public:
-    VariableReader() :
-        input(), variable(0), variableName("") {
+	VariableReader() :
+			input(), variable(0), variableName("") {
 
-    }
+	}
 
-    VariableReader(TChainPointer chain, TString varName) :
-        input(chain), variable(0), variableName(varName) {
+	VariableReader(TChainPointer chain, TString varName) :
+			input(chain), variable(0), variableName(varName) {
 
-    }
+	}
 
-    ~VariableReader() {
-    }
+	~VariableReader() {
+	}
 
-    const variableType& getVariable() {
-        return variable;
-    }
+	const variableType& getVariable() {
+		return variable;
+	}
 
-    double getVariableAt(unsigned int index) const;
-    int getIntVariableAt(unsigned int index) const;
-    unsigned int getUIntVariableAt(unsigned int index) const;
-    bool getBoolVariableAt(unsigned int index) const;
-    unsigned int size() const;
+	double getVariableAt(unsigned int index) const;
+	int getIntVariableAt(unsigned int index) const;
+	unsigned int getUIntVariableAt(unsigned int index) const;
+	bool getBoolVariableAt(unsigned int index) const;
+	unsigned int size() const;
 
-    void initialise() {
-        if (doesVariableExist()) {
-            enableVariable();
-            readVariableFromInput();
-        } else
-            throw VariableNotFoundException("Variable '" + variableName + "' was not found.");
-    }
+	void initialise() {
+		if (doesVariableExist()) {
+			enableVariable();
+			readVariableFromInput();
+		} else
+			throw VariableNotFoundException(
+					"Variable '" + variableName + "' was not found.\n"
+							+ "Please check the file content and/or NTuple version.");
+	}
 
-    void initialiseBlindly() {
-        enableVariable();
-        readVariableFromInput();
-    }
+	void initialiseBlindly() {
+		enableVariable();
+		readVariableFromInput();
+	}
 
-    bool doesVariableExist() {
-        return input->GetBranch(variableName) != NULL;
-    }
+	bool doesVariableExist() {
+		return input->GetBranch(variableName) != NULL;
+	}
 
-    void throwExceptionIfNotItitialised() const{
-        if (variable == 0)
-            throw VariableNotFoundException("Trying to access variable '" + variableName + "' before initialisation.");
-    }
+	void throwExceptionIfNotItitialised() const {
+		if (variable == 0)
+			throw VariableNotFoundException("Trying to access variable '" + variableName + "' before initialisation.");
+	}
 protected:
-    TChainPointer input;
-    variableType variable;
-    TString variableName;
+	TChainPointer input;
+	variableType variable;
+	TString variableName;
 
-    void readVariableFromInput() {
-        input->SetBranchAddress(variableName, &variable);
-    }
+	void readVariableFromInput() {
+		input->SetBranchAddress(variableName, &variable);
+	}
 
-    void enableVariable() {
-        input->SetBranchStatus(variableName, true);
-    }
+	void enableVariable() {
+		input->SetBranchStatus(variableName, true);
+	}
 };
 
 }
