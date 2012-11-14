64ea805d90f7a7fdfa40870b1a94c7526727dd25
kreczko <lkreczko@gmail.com>
2012-11-14 12:33:50 +0000

    added makefiles

---
+++ Profile/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_filesystem -lboost_system -lboost_python -lboost_program_options -lpython2.7 -lCLHEP -lTopQuarkAnalysis -lTreePlayer
+
