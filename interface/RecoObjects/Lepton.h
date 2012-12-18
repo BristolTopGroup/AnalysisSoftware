c9f1fed2c56bfc8667b5584031f59fc53baceb4b
phy6phs <philip.hugh.symonds@cern.ch>
2012-12-18 23:32:04 +0000

    reading new reliso vars

--- interface/RecoObjects/Lepton.h
+++ interface/RecoObjects/Lepton.h
@@ -34,7 +34,10 @@ public:
 	virtual void setPFIsolationWithGaussianFallOff(double iso, double coneSize = 0.3);
 	virtual void setPFRelativeIsolationRho(double isolation);
 	virtual void setPFDeltaBeta_Isolation_DR04(double isolation);
-
+	virtual void setsumChargedHadronPt04(double isolation);
+	virtual void setsumNeutralHadronPt04(double isolation);
+	virtual void setsumPhotonPt04(double isolation);
+	virtual void setsumPUPt04(double isolation);
 	//directional particle based isolation
 	virtual void setDirectionalIsolation(double iso, double coneSize = 0.3);
 	virtual void setDirectionalIsolationWithGaussianFallOff(double iso, double coneSize = 0.3);
@@ -57,6 +60,10 @@ public:
 	virtual double pfRelativeIsolationPUCorrected(double rho, double coneSize = 0.3) const;
 	virtual double pfIsolationWithGaussianFallOff(double coneSize = 0.3) const;
 	virtual double PFDeltaBeta_Isolation_DR04() const;
+	virtual double sumChargedHadronPt04() const;
+	virtual double sumNeutralHadronPt04() const;
+	virtual double sumPhotonPt04() const;
+	virtual double sumPUPt04() const;
 	//directional particle based isolation
 	virtual double directionalIsolation(double coneSize = 0.2) const;
 	virtual double directionalIsolationWithGaussianFallOff(double coneSize = 0.2) const;
@@ -72,6 +79,7 @@ protected:
 	double PFGamma_Isolation_DR04_, PFChargedHadron_Isolation_DR04_, PFNeutralHadron_Isolation_DR04_, PFPUChargedHadron_Isolation_DR04_;
 	double PFGamma_Isolation_DR05_, PFChargedHadron_Isolation_DR05_, PFNeutralHadron_Isolation_DR05_, PFPUChargedHadron_Isolation_DR05_;
 	double PFDeltaBeta_Isolation_DR04_;
+	double sumChargedHadronPt04_, sumNeutralHadronPt04_, sumPhotonPt04_, sumPUPt04_;
 	double PFRelativeIsolationRho_DR03_;
 	double zDistanceToPrimaryVertex_, directionalIsolation_DR02_, directionalIsolation_DR03_;
 	double  directionalIsolation_GaussianFallOff_DR02_, directionalIsolation_GaussianFallOff_DR03_;
