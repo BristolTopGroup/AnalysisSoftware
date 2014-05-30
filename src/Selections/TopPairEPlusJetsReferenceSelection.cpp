c7e27a53a645547b380aa662974a26558e921f94
Emyr Clement <emyr.clement@bristol.ac.uk>
2014-05-30 15:59:13 +0100

    Remove (commented out) HOverE cut.

--- src/Selections/TopPairEPlusJetsReferenceSelection.cpp
+++ src/Selections/TopPairEPlusJetsReferenceSelection.cpp
@@ -163,12 +163,8 @@ bool TopPairEPlusJetsReferenceSelection::hasExactlyOneIsolatedLepton(const Event
 bool TopPairEPlusJetsReferenceSelection::isGoodElectron(const ElectronPointer electron) const {
 	bool passesEtAndEta = electron->et() > 30 && fabs(electron->eta()) < 2.5 && !electron->isInCrack();
 	bool passesD0 = fabs(electron->d0()) < 0.02; //cm
-			//since H/E is used at trigger level, use the same cut here:
-//	bool passesHOverE = electron->HadOverEm() < 0.05; // same for EE and EB
-	bool passesHOverE = true; // Synch with unfolding workflow
 	bool passesID(electron->passesElectronID(ElectronID::MVAIDTrigger));
-	return passesEtAndEta && passesD0 &&
-			passesHOverE && passesID;
+	return passesEtAndEta && passesD0 && passesID;
 }
 
 bool TopPairEPlusJetsReferenceSelection::isIsolated(const LeptonPointer lepton) const {
