672e5a87909a5121ce9f6cbab05266e860445a7d
Lukasz Kreczko <lkreczko@googlemail.com>
2014-08-22 17:30:20 +0100

    adding first simple test to CMake config!!

--- test/simpletest.cpp
+++ test/simpletest.cpp
@@ -1,87 +1,15 @@
-/*********************************************************************************
- * This file is part of CUTE.
- *
- * CUTE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * CUTE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with CUTE.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright 2007 Peter Sommerlad
- *
- *********************************************************************************/
-
-#include <iostream>
-#include "cute_runner.h"
-#include "ide_listener.h"
-#include "cute.h"
-
-int lifeTheUniverseAndEverything = 42;
-
-void mySimpleTest() {
-	ASSERT_EQUAL(lifeTheUniverseAndEverything, 6 * 7);
+#define BOOST_TEST_MODULE SimpleTest
+#include <boost/test/included/unit_test.hpp>
+int add(int i, int j) {
+	return i + j;
 }
 
-void example1() {
-	using namespace std;
-	cute::test successfulTest = CUTE(mySimpleTest);
-	cute::ide_listener<> lis;
-	cute::runner<cute::ide_listener<> > run = cute::makeRunner(lis);
-	if (run(successfulTest)) {
-		cout << "OK" << endl;
-	} else {
-		cout << "failed" << endl;
-	}
+BOOST_AUTO_TEST_SUITE(SimpleTest)
+BOOST_AUTO_TEST_CASE( one_and_one ) {
+	BOOST_REQUIRE(add(1, 1) == 2);
 }
 
-void example2() {
-	using namespace std;
-	cute::ide_listener<> lis;
-	cute::makeRunner(lis)(CUTE(mySimpleTest));
+BOOST_AUTO_TEST_CASE( fail ) {
+	BOOST_REQUIRE(add(1, 1) == 3);
 }
-
-int anotherTest() {
-	ASSERT_EQUAL(42, lifeTheUniverseAndEverything);
-	return 0;
-}
-
-cute::test tests[] = {
-#ifdef __GNUG__
-		CUTE(mySimpleTest), mySimpleTest, CUTE(anotherTest)
-#else /* for MSVC... */
-		CUTE(mySimpleTest)
-		,CUTE(mySimpleTest)
-		,CUTE(reinterpret_cast<void(*)()>(anotherTest))
-#endif
-	};
-
-struct ATestFunctor {
-	void operator()() {
-		ASSERT_EQUAL_DELTA(42.0,
-				static_cast<double>(lifeTheUniverseAndEverything), 0.001);
-	}
-};
-
-void example3() {
-	using namespace std;
-	cute::ide_listener<> lis;
-	cute::runner<cute::ide_listener<> > run = cute::makeRunner(lis);
-	cute::suite s(tests, tests + (sizeof(tests) / sizeof(tests[0])));
-	s += ATestFunctor();
-	cute::test failingTest = cute::test(failingTestWithMessage);
-	s += failingTest;
-	run(s, "suite");
-}
-
-int main() {
-	example1();
-	example2();
-	example3();
-}
+BOOST_AUTO_TEST_SUITE_END( )
