c4f62e42efa866b1cf79cb07133fbdca0d32d5f0
Lukasz Kreczko <lkreczko@googlemail.com>
2014-12-11 14:03:29 +0100

    checkpoint

--- condor/job.sh
+++ condor/job.sh
@@ -3,6 +3,10 @@
 exe=BAT
 toolsFolder=BristolAnalysis/Tools/
 VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+host=`hostname`
+# some information for debugging
+echo "I am running on $host"
+echo "I got the following parameters: $@"
 # source CMSSW env
 . $VO_CMS_SW_DIR/cmsset_default.sh
 # get CMSSW
@@ -14,9 +18,14 @@ tar -xf ../../Analysis.tar
 # scram b -j8 #needs to be steered by condor.sh
 # otherwise we risk to be evicted
 scram b
+echo "I will run the following executable now:"
 which $exe
 
-sample="QCD_80to170_BCtoE" analysisMode="central" nohup ${exe} ${toolsFolder}python/master_2012_cfg.py ${TQAFPath} &> QCD_80to170_BCtoE_central.log &
+#figure out what I want to run
+sample=BristolAnalysis/Tools/condor/job_mapper "$@" --return_sample
+analysisMode=BristolAnalysis/Tools/condor/job_mapper "$@" --return_mode
+echo "I will run sample=${sample} in mode=${analysisMode}"
+${exe} ${toolsFolder}python/master_2012_cfg.py ${TQAFPath} &> ${sample}_${analysisMode}.log &
 wait
 # copy outputs to initial job directory (everything else is ignored)
 cp *.log ../../.
