cec9bd30dbc6dd6f26eb1def1d64f43f782004c0
kreczko <lkreczko@googlemail.com>
2015-02-02 13:34:08 +0000

    added better test

--- test/DataTypeTests.cpp
+++ test/DataTypeTests.cpp
@@ -3,23 +3,40 @@
 #include "../interface/DataTypes.h"
 #include <string>
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+
+using namespace BAT;
 
 BOOST_AUTO_TEST_SUITE(DataTypeTests)
-BOOST_AUTO_TEST_CASE( NoSimilarDataTypes ) {
-	for (auto i = 0; i < BAT::DataType::names.size(); ++i) {
-		std::string name1 = BAT::DataType::names.at(i);
-		for (auto j = 0; j < BAT::DataType::names.size(); ++j) {
-			std::string name2 = BAT::DataType::names.at(j);
-			if (name1 != name2) {
+
+BOOST_AUTO_TEST_CASE( AllDataTypesHaveMapping ) {
+	BOOST_REQUIRE(DataType::NUMBER_OF_DATA_TYPES == DataType::names.size());
+}
+
+BOOST_AUTO_TEST_CASE( NoAmbiguousMappingSoft ) {
+	for (auto i = 0; i < DataType::names.size(); ++i) {
+		DataType::value name1 = DataType::getDataType(DataType::names.at(i));
+		for (auto j = 0; j < DataType::names.size(); ++j) {
+			DataType::value name2 = DataType::getDataType(DataType::names.at(j));
+			if (i != j) {
 				// there should be no overlap with sub-strings
-				BOOST_REQUIRE_MESSAGE(!boost::starts_with(name1, name2), name1 << " and " << name2 << " are similar");
+				BOOST_REQUIRE_MESSAGE(name1 != name2, name1 << " and " << name2 << " are similar");
 			}
 		}
 	}
 }
 
-BOOST_AUTO_TEST_CASE( AllDataTypesHaveMapping ) {
-	BOOST_REQUIRE(BAT::DataType::NUMBER_OF_DATA_TYPES == BAT::DataType::names.size());
-
+BOOST_AUTO_TEST_CASE( NoAmbiguousMappingHard ) {
+	for (auto i = (DataType::names.size()); i-- > 0;) {
+		DataType::names.at(0);
+		DataType::value name1 = DataType::getDataType(DataType::names.at(i));
+		for (auto j = 0; j < DataType::names.size(); ++j) {
+			DataType::value name2 = DataType::getDataType(DataType::names.at(j));
+			if (i != j) {
+				// there should be no overlap with sub-strings
+				BOOST_REQUIRE_MESSAGE(name1 != name2, name1 << " and " << name2 << " are similar");
+			}
+		}
+	}
 }
 BOOST_AUTO_TEST_SUITE_END( )
