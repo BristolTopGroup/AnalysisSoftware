b764738c7c1e6b3e5c148cfd9d91928bbf65b801
kreczko <lkreczko@googlemail.com>
2015-02-23 16:15:41 +0000

    added WAnalyser (@EmyrClement)

---
+++ interface/Analysers/WAnalyser.h
@@ -0,0 +1,31 @@
+/*
+ * WAnalyser.h
+ *
+ *  Created on: 12 Jan 2015
+ *      Author: ejclemen
+ */
+
+#ifndef WANALYSER_H_
+#define WANALYSER_H_
+
+#include "BasicAnalyser.h"
+
+namespace BAT {
+
+class WAnalyser: public BAT::BasicAnalyser {
+public:
+	WAnalyser(HistogramManagerPtr histMan, std::string histogramFolder = "WAnalysis");
+	virtual ~WAnalyser();
+	void analyse(const EventPtr);
+	void analyseHadronicW(const EventPtr, const JetCollection jets, const JetCollection bjets);
+	void analyseHadronicW_partons(const EventPtr);
+
+	void createHistograms();
+
+protected:
+	void fillHistograms(std::string subcollection, std::string suffix = "");
+};
+typedef boost::scoped_ptr<WAnalyser> WAnalyserLocalPtr;
+typedef boost::shared_ptr<WAnalyser> WAnalyserPtr;
+} /* namespace BAT */
+#endif /* WANALYSER_H_ */
