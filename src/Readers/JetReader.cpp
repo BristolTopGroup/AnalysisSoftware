f0576c20b5c29efd037779163df7b45a886f53dc
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-04-20 09:31:43 +0100

    Change JES Uncertainty to 2% for time being.

--- src/Readers/JetReader.cpp
+++ src/Readers/JetReader.cpp
@@ -113,7 +113,7 @@ void JetReader::readJets(bool isRealData) {
 
 		double JEC = JECReader.getVariableAt(jetIndex);
 		double JECUnc = JECUncReader.getVariableAt(jetIndex);
-		JECUnc = 0.03;
+		JECUnc = 0.02;
 		// cout << Globals::JESsystematic << " " << JECUnc << endl;
 		//applying JES + or - systematic, 0 by default)
 		energy = energy * (1+JECUnc*Globals::JESsystematic);
