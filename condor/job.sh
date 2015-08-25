99eb4259d9b3caf4812231d4377512a08fbc951d
jjacob <jeson.jacob@cern.ch>
2015-08-25 10:58:31 +0100

    Pulling in latest updates from master branch for running BAT jobs on DICE.

--- condor/job.sh
+++ condor/job.sh
@@ -26,6 +26,9 @@ n_cores=`BristolAnalysis/Tools/condor/job_mapper "$@"  --return_cores`
 # if n_cores > 1 we need to do a for loop and overwrite 
 # the process parameter
 process=`BristolAnalysis/Tools/condor/job_mapper "$@" --return_process`
+# TODO: FIXME: create CMSSW area based on this variable
+# the tricky part: job_mapper cannot be used before setting up CMSSW
+cmssw_version=`BristolAnalysis/Tools/condor/job_mapper "$@"  --return_cmssw`
 local_process=$process
 range_for_loop=`expr "$n_cores" + "$process"`
 while [ $local_process -lt $range_for_loop ] ; do
@@ -33,24 +36,36 @@ while [ $local_process -lt $range_for_loop ] ; do
 	sample=`BristolAnalysis/Tools/condor/job_mapper "$@" --return_sample --process $local_process`
 	analysisMode=`BristolAnalysis/Tools/condor/job_mapper "$@" --return_mode --process $local_process`
 	energy=`BristolAnalysis/Tools/condor/job_mapper "$@"  --return_energy`
-	cmssw_version=`BristolAnalysis/Tools/condor/job_mapper "$@"  --return_cmssw`
 	operation=`BristolAnalysis/Tools/condor/job_mapper "$@" --return_operation`
-	echo "I will run sample=${sample} in mode=${analysisMode} for centre-of-mass energy of ${energy} TeV"
+	splitTTJet=`BristolAnalysis/Tools/condor/job_mapper "$@" --return_splitTTJet`
+	ntupleToProcess=`BristolAnalysis/Tools/condor/job_mapper "$@" --return_ntupleToProcess --process $local_process`
 
-	python_config=master_2012_cfg.py
+	echo "I will run sample=${sample} in mode=${analysisMode} for centre-of-mass energy of ${energy} TeV"
+	echo "Analysing ntuple number "$ntupleToProcess
+
+	python_config=master_2015_cfg.py
 	if [ $energy -eq 7 ]; then
 		python_config=master_2011_53X_cfg.py
+	fi
+	if [ $energy -eq 8 ]; then
+		python_config=master_2012_cfg.py
 	fi
 	if [ $operation == "test" ]; then
 		python_config=test_cfg.py
 	fi
 
 	log_file=${sample}_${analysisMode}_${energy}TeV_${cmssw_version}.log
-	sample="$sample" analysisMode="$analysisMode" ${exe} ${toolsFolder}python/${python_config} ${TQAFPath} &> $log_file &
+	if [ $ntupleToProcess -ne -1 ]; then
+		log_file=${sample}_${analysisMode}_${ntupleToProcess}_${energy}TeV_${cmssw_version}.log
+	fi
+
+	sample="$sample" analysisMode="$analysisMode" ntupleToProcess=$ntupleToProcess ${exe} ${toolsFolder}python/${python_config} ${TQAFPath} &> $log_file &
 	
 	let local_process+=1
 done
 wait
+echo "ls"
+ls -trlh
 echo "All done"
 # copy outputs to initial job directory (everything else is ignored)
 cp *.log ../../.
