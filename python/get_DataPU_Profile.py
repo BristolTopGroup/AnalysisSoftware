f2221e30934c782d441c87dd3e8f2dfde721304a
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-11-18 10:30:51 +0000

    New recommendation for minbias xsec.

--- python/get_DataPU_Profile.py
+++ python/get_DataPU_Profile.py
@@ -3,7 +3,7 @@ import subprocess
 
 jsonFile = '/hdfs/TopQuarkGroup/run2/json/Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON_v3.txt'
 pileupJson = '/hdfs/TopQuarkGroup/run2/json/pileup/pileup_JSON_11-02-2015.txt'
-minBiasXsec = 80000
+minBiasXsec = 69000
 uncertainty = 0.05#%
 
 variations = {
