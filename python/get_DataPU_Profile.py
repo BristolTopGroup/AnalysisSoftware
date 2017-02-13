e1b97e83829cc8cf86c822fab2f496f0ce8a130b
Emyr Clement <emyr.clement@bristol.ac.uk>
2017-02-13 13:32:13 +0000

    Update PU distributions.

--- python/get_DataPU_Profile.py
+++ python/get_DataPU_Profile.py
@@ -9,7 +9,7 @@ import subprocess
 jsonFile = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
 pileupJson = 'pileup_latest.txt'
 
-minBiasXsec = 69200
+minBiasXsec = 69000
 uncertainty = 0.049#%
 
 variations = {
